06cc2fb369e7432ca0091edd615e2094be832424
CeliaGallegoGomez <cela17@hotmail.es>
2024-05-21 18:49:27 +0200

    UPDATE

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,35 @@
-// El styles lo importamos aquí, ya se carga después al compilar todo
-import '../scss/styles.scss';
+import "../scss/styles.scss";
+
+//ejemplo
+
+// const names = ["Keko", "Cristina", "Celia", "Andrea"];
+// for (let counter = 0; counter < names.length; counter++) {
+//   console.log(names[counter]);
+// }
+
+// a la inversa
+
+// const names = ["Keko", "Cristina", "Celia", "Andrea"];
+// for (let counter = names.length - 1; counter >= 0; counter--) {
+//   console.log(names[counter]);
+// }
+
+// otro tipo de bucle
+
+// for (const name of names) {
+//   console.log(name);
+// }
+
+// - Crea una función que reciba un número, la función debe imprimir una cuenta atrás desde ese número hasta 0.
+
+for (let counter = 10; counter >= 0; counter--) {
+  console.log(counter);
+}
+
+// - Crea una función que reciba un array de 10 números e imprima por consola la suma de todos los valores del array.
+
+const numbers [1,2,3,4,5,6,7,8,9,10] {
+    for (let counter);
+};
+
+
